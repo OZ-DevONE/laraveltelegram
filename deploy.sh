07a7d84656ae25724daf71c4296668144b30e1ba
OZ-DevONE <zajnutdinovramazan@gmail.com>
2023-12-14 00:54:10 +0500

    fix sh

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,16 @@ echo "Start Dep..."
 
 git pull origin main
 
-php8.2 artisan down
-
 php8.2 composer.phar install --no-dev --optimize-autoloader
 
 php8.2 artisan migrate 
 
-php8.2 artisan up
+php8.2 artisan config:cache 
+
+php8.2 artisan event:cache 
+
+php8.2 artisan route:cache 
+
+php8.2 artisan view:cache 
 
 echo "Done!"
