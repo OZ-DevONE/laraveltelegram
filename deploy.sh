8fae3cc0cc6ea25b60f93ef23ae3972925587563
OZ-DevONE <zajnutdinovramazan@gmail.com>
2023-12-14 02:32:57 +0500

    add cache

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,14 @@ php8.2 composer.phar install --no-dev --optimize-autoloader
 
 php8.2 artisan migrate 
 
+php8.2 artisan config:cache 
+
+php8.2 artisan event:cache 
+
+php8.2 artisan route:cache 
+
+php8.2 artisan view:cache 
+
 php8.2 artisan up
 
 echo "Done!"
