c1e9eb4a61e036bbb8b11775f00f043e84d20d01
OZ-DevONE <zajnutdinovramazan@gmail.com>
2023-12-15 19:07:30 +0500

    add send gif and fix text

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,8 +21,8 @@ class HomeController extends Controller
     public function sendToAllChats(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'text' => ['required', 'string', 'max:255', 'regex:/^[a-zA-Z0-9\s.,!?-]+$/'],
-            'image' => ['url', 'max:255'], 
+            'text' => ['required', 'string', 'max:255', 'regex:/^[\p{L}\p{N}\p{P}\p{Zs}]+$/u'],
+            'image' => ['required', 'url', 'regex:/\.(jpeg|jpg|png|gif|mp4)$/i'], 
         ]);
     
         // Проверка на ошибки валидации
@@ -36,20 +36,45 @@ class HomeController extends Controller
         $activeChats = TelegramGroup::where('is_active', true)->get();
 
         foreach ($activeChats as $chat) {
+            // Отправка текстового сообщения
             Telegram::sendMessage([
                 'chat_id' => $chat->chat_id,
                 'text' => $text
             ]);
-
+        
             if ($imageUrl) {
-                // Загрузка и отправка изображения
-                $image = InputFile::create($imageUrl, basename($imageUrl));
-                Telegram::sendPhoto([
-                    'chat_id' => $chat->chat_id,
-                    'photo' => $image
-                ]);
+                // Определение типа медиа
+                $extension = strtolower(pathinfo($imageUrl, PATHINFO_EXTENSION));
+                switch ($extension) {
+                    case 'jpg':
+                    case 'jpeg':
+                    case 'png':
+                        // Отправка изображения
+                        $image = InputFile::create($imageUrl, basename($imageUrl));
+                        Telegram::sendPhoto([
+                            'chat_id' => $chat->chat_id,
+                            'photo' => $image
+                        ]);
+                        break;
+                    case 'gif':
+                        // Отправка GIF
+                        $gif = InputFile::create($imageUrl, basename($imageUrl));
+                        Telegram::sendAnimation([
+                            'chat_id' => $chat->chat_id,
+                            'animation' => $gif
+                        ]);
+                        break;
+                    case 'mp4':
+                        // Отправка видео
+                        $video = InputFile::create($imageUrl, basename($imageUrl));
+                        Telegram::sendVideo([
+                            'chat_id' => $chat->chat_id,
+                            'video' => $video
+                        ]);
+                        break;
+                }
             }
-        }
+        }              
 
         return redirect()->back()->with('status', 'Сообщение отправлено во все активные чаты');
     }
