b549aab7316ecdf0f73e2a31283c8931c20f0c58
OZ-DevONE <zajnutdinovramazan@gmail.com>
2023-12-20 01:17:54 +0500

    test admin funct

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -6,6 +6,8 @@ use Telegram\Bot\Laravel\Facades\Telegram;
 
 class TelegramController extends Controller
 {
+    protected $activityLog = [];
+
     public function webhook()
     {
         $update = Telegram::commandsHandler(true);
@@ -65,18 +67,92 @@ class TelegramController extends Controller
             }
         }
 
+        if ($message) {
+            $chatId = $message->getChat()->getId();
+            $userId = $message->getFrom()->getId();
+            $text = $message->getText();
+            $isSticker = $message->getSticker() ? true : false;
+            $containsLink = $this->containsLink($text);
+
+            $this->updateUserActivity($userId, $chatId, $isSticker, $containsLink);
+
+            if ($this->isUserOverActive($userId, $chatId)) {
+                $this->deleteRecentMessages($userId, $chatId);
+            }
+        }
+
         return response()->json(['status' => 'success']);
     }
 
     private function containsBadWords($text)
     {
-        $badWords = ['блять', 'сука', 'хуйло', 'типа матное слово'];
+        $badWords = ['блять', 'сука', 'хуйло', 'типа матное слово', 'хуй', 'пидорас', 'пидор', 'хуеглот', ''];
         foreach ($badWords as $word) {
             if (stripos($text, $word) !== false) {
                 return true;
             }
         }
         return false;
+    }
+
+    private function containsLink($text)
+    {
+        return preg_match('/\bhttps?:\/\/\S+/i', $text);
+    }
+
+    private function updateUserActivity($userId, $chatId, $isSticker, $containsLink)
+    {
+        $currentTime = time();
+
+        // Очищаем устаревшие записи
+        foreach ($this->activityLog as $key => $log) {
+            if ($currentTime - $log['time'] > 300) { // 300 секунд = 5 минут
+                unset($this->activityLog[$key]);
+            }
+        }
+
+        // Добавляем новую запись
+        $this->activityLog[] = [
+            'userId' => $userId,
+            'chatId' => $chatId,
+            'isSticker' => $isSticker,
+            'containsLink' => $containsLink,
+            'time' => $currentTime
+        ];
+    }
+
+    private function isUserOverActive($userId, $chatId)
+    {
+        $countMessages = 0;
+        $countStickers = 0;
+        $countLinkMessages = 0;
+
+        foreach ($this->activityLog as $log) {
+            if ($log['userId'] == $userId && $log['chatId'] == $chatId) {
+                $countMessages++;
+                if ($log['isSticker']) {
+                    $countStickers++;
+                }
+                if ($log['containsLink']) {
+                    $countLinkMessages++;
+                }
+            }
+        }
+
+        // Условия для определения подозрительной активности
+        return $countMessages > 10 || $countStickers > 5 || $countLinkMessages > 3;
+    }
+
+    private function deleteRecentMessages($userId, $chatId)
+    {
+        foreach ($this->activityLog as $log) {
+            if ($log['userId'] == $userId && $log['chatId'] == $chatId) {
+                Telegram::deleteMessage([
+                    'chat_id' => $chatId,
+                    'message_id' => $log['messageId']
+                ]);
+            }
+        }
     }
     
 }
